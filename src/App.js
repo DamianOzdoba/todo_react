1b8ecf3d0c37c3c6987c70219e16e123a3d196a9
DamianOzdoba <ozdoba.damian@gmail.com>
2022-04-13 13:06:08 +0200

    most important react functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Todo</h1>
-    </div>
-  );
+    const [value, setValue] = useState('');
+    const [tasks, setTasks] = useState([]);
+
+
+    const handleInput = (event) => {
+        setValue(event.target.value);
+    }
+
+    const handleAddTask = (event) => {
+        if (event.key === 'Enter'){
+            setTasks([...tasks, value]);
+            setValue('');
+        }
+    }
+
+    return (
+        <div className="App">
+            <h1>Todo</h1>
+            <input
+                type='text'
+                value={value}
+                onChange={handleInput}
+                onKeyUp={handleAddTask}
+            />
+            <h2>{value}</h2>
+        </div>
+    );
 }
 
 export default App;
